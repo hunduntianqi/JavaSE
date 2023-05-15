4562a277c238243a702448680c9edf3e4683c067
混沌天炁 <83585673+hunduntianqi@users.noreply.github.com>
2023-05-15 19:21:37 +0800

    Add files via upload

---
+++ src/main/java/com/$2_First_Java_code/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.$2_First_Java_code;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 输出"hello world"
+        System.out.println("hello world");
+    }
+}
